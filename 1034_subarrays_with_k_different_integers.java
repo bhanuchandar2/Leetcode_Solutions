58aa475cf0d0c9514e4af468a5e69d6e749a83b9
bhanuchandar2 <bhanu02b@gmail.com>
2026-08-15 12:41:16 +0530

    Add 1034_subarrays_with_k_different_integers.java - LeetCode Problem 1034

---
+++ 1034_subarrays_with_k_different_integers.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        int answer=atMost(nums,k)-atMost(nums,k-1);
+        return answer;
+    }
+    public static int atMost(int[] nums, int k) {
+    int l = 0;
+    int count = 0;
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for (int r = 0; r < nums.length; r++) {
+        map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
+
+        while (map.size() > k) {
+            map.put(nums[l], map.get(nums[l]) - 1);
+
+            if (map.get(nums[l]) == 0) {
+                map.remove(nums[l]);
+            }
+
+            l++;
+        }
+
+        count += r - l + 1;
+    }
+
+    return count;
+}
+}
