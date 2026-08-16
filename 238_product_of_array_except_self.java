99a62bf20bb2ee3230464fbc8da980366c7b4e8c
bhanuchandar2 <bhanu02b@gmail.com>
2026-08-16 15:16:59 +0530

    Add 238_product_of_array_except_self.java - LeetCode Problem 238

---
+++ 238_product_of_array_except_self.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int leftp[]=new int[nums.length];
+       int rightp[]=new int[nums.length];
+       leftp[0]=1;
+       rightp[nums.length-1]=1;
+       for(int i=1;i<nums.length;i++){
+        leftp[i]=leftp[i-1]*nums[i-1];
+       }
+       for(int i=nums.length-2;i>=0;i--){
+        rightp[i]=rightp[i+1]*nums[i+1];
+       }
+       for(int i=0;i<nums.length;i++){
+        nums[i]=leftp[i]*rightp[i];
+       }
+       return nums;
+    }
+}
