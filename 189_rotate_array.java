bc8f743a7646040d2bc4c6e497affcc620d68b10
bhanuchandar2 <bhanu02b@gmail.com>
2026-09-13 12:19:45 +0530

    Add 189_rotate_array.java - LeetCode Problem 189

---
+++ 189_rotate_array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k=k%nums.length;
+        reverse(0,nums.length-1,nums);
+        reverse(0,k-1,nums);
+        reverse(k,nums.length-1,nums);
+    }
+
+    public void reverse(int start,int end,int arr[]){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
